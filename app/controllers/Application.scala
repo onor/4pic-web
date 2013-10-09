a398be1255dca56c0f6075d439776bae559cfafb
Rudolf Markulin <rudolf.markulin@gmail.com>
2013-10-09 16:56:11 +0200

    gameKey refactor

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,68 +5,55 @@ import play.api.Logger
 import play.api.libs.ws.WS
 import play.api.libs.concurrent.Execution.Implicits._
 
-object Application extends Controller {
+object Application extends Controller with GameController {
 
-  val onorUrl = play.api.Play.current.configuration.getString("onorplatform.url").get
-  val userKey = "4b1469e3ff90b438ef0134b1cb266c06"
-  val GAMEKEY = "gameKey"
-
-  def index = Action { implicit request =>
-    Ok(views.html.index())
+  def index = Action {
+    implicit request =>
+      Ok(views.html.index())
   }
 
-  def indexPost(gameKey:Int) = Action { implicit request =>
-    Logger.info("INDEX POST")
+  def indexPost(gameKey: Int) = Action {
+    implicit request =>
+      Logger.info("INDEX POST")
 
-    def callback(gameKey:Int) = s"http://${request.host}/gameKey/$gameKey/facebook/login"
+      def callback(gameKey: Int) = s"http://${request.host}/gameKey/$gameKey/facebook/login"
 
-    val settings = Facebook.facebookSettings(gameKey)
-    val sr = request.body.asFormUrlEncoded.get("signed_request").head
-    components.SignedRequestUtils.parseSignedRequest(sr, settings.appSecret) match {
-      case Some(signedRequest) => {
-        Logger.info("GOT SIGNED REQUEST")
-        Redirect(routes.Application.index()).withSession(("fbid", signedRequest.user_id), (GAMEKEY, gameKey.toString))
-      }
-      case None => {
-        Logger.info("DIDNT GET SIGNED REQUEST")
-        Ok(views.html.redirect(settings.appId, callback(gameKey), Facebook.fscope))
-      }
-    }
-  }
-
-  def game = Action { implicit request  =>
-    request.session.get(GAMEKEY).map(_.toInt) match {
-      case Some(gameKey) => Async {
-          WS.
-            url(s"$onorUrl/client/v1/games/4pics1word/$gameKey?userKey=$userKey").
-            get.map(res => Ok(res.json))
+      val settings = Facebook.facebookSettings(gameKey)
+      val sr = request.body.asFormUrlEncoded.get("signed_request").head
+      components.SignedRequestUtils.parseSignedRequest(sr, settings.appSecret) match {
+        case Some(signedRequest) => {
+          Logger.info("GOT SIGNED REQUEST")
+          Redirect(routes.Application.index()).withSession(("fbid", signedRequest.user_id), (GAMEKEY, gameKey.toString))
         }
-
-      case None => BadRequest("")
-    }
-  }
-
-  def angularConfig = Action { implicit request =>
-    request.session.get(GAMEKEY).map(_.toInt) match {
-      case Some(gameKey) => {
-        val settings = Facebook.facebookSettings(gameKey)
-        Ok(views.txt.config(settings.appId))
+        case None => {
+          Logger.info("DIDNT GET SIGNED REQUEST")
+          Ok(views.html.redirect(settings.appId, callback(gameKey), Facebook.fscope))
+        }
       }
-
-      case None => BadRequest("")
-    }
   }
 
-  def appCss = Action { implicit request =>
-    request.session.get(GAMEKEY).map(_.toInt) match {
-      case Some(gameKey) => Async {
+  def game = WithGameKey(p = parse.anyContent) {
+    implicit request =>
+      Async {
         WS.
-          url(s"$onorUrl/client/v1/games/4pics1word/$gameKey?userKey=$userKey").
+          url(s"$onorUrl/client/v1/games/4pics1word/${request.gameKey}?userKey=$userKey").
+          get.map(res => Ok(res.json))
+      }
+  }
+
+  def angularConfig = WithGameKey(p = parse.anyContent) {
+    implicit request =>
+      val settings = Facebook.facebookSettings(request.gameKey)
+      Ok(views.txt.config(settings.appId))
+  }
+
+  def appCss = WithGameKey(p = parse.anyContent) {
+    implicit request =>
+      Async {
+        WS.
+          url(s"$onorUrl/client/v1/games/4pics1word/${request.gameKey}?userKey=$userKey").
           get.map(res => Ok(views.txt.app((res.json \ "backgroundUrl").as[String])).as(CSS))
       }
-
-      case None => BadRequest("")
-    }
   }
 
 }
--- app/controllers/ScoreService.scala
+++ app/controllers/ScoreService.scala
@@ -5,64 +5,53 @@ import play.api.Logger
 import play.api.libs.ws.WS
 import play.api.libs.concurrent.Execution.Implicits._
 
-object ScoreService extends Controller {
+object ScoreService extends Controller with GameController {
 
-  val onorUrl = play.api.Play.current.configuration.getString("onorplatform.url").get
-  val userKey = "4b1469e3ff90b438ef0134b1cb266c06"
-  val GAMEKEY = "gameKey"
-
-  def show(levelPack:Int) = Action { implicit request  =>
-    request.session.get(GAMEKEY).map(_.toInt) match {
-      case Some(gameKey) => Async {
+  def show(levelPack: Int) = WithGameKey(p = parse.anyContent) {
+    implicit request =>
+      Async {
         val leaderboardKey = "todo"
         val id = request.session.get("fbid")
         WS.
           url(s"$onorUrl/client/v1/scores/ld/$leaderboardKey/user/$id/provider/facebook?userKey=$userKey").
           get.map(res => {
-          if(res.status == 200)
+          if (res.status == 200)
             Ok(res.json)
           else BadRequest(res.body)
         })
       }
-
-      case None => BadRequest("")
-    }
   }
 
-  def create(levelPack:Int) = Action { implicit request  =>
-    request.session.get(GAMEKEY).map(_.toInt) match {
-      case Some(gameKey) => Async {
+  def create(levelPack: Int) = WithGameKey(p = parse.anyContent) {
+    implicit request =>
+      Async {
         val leaderboardKey = "todo"
         val id = request.session.get("fbid")
         WS.
           url(s"$onorUrl/client/v1/scores/ld/$leaderboardKey/user/$id/provider/facebook?userKey=$userKey").
-          post(request.body.asJson.get).map(res => { //todo move get
-          if(res.status == 200)
+          post(request.body.asJson.get).map(res => {
+          //todo move get
+          if (res.status == 200)
             Ok(res.json)
           else BadRequest(res.body)
         })
       }
-
-      case None => BadRequest("")
-    }
   }
 
-  def update(levelPack:Int) = Action { implicit request  =>
-    request.session.get(GAMEKEY).map(_.toInt) match {
-      case Some(gameKey) => Async {
+  def update(levelPack: Int) = WithGameKey(p = parse.anyContent) {
+    implicit request =>
+      Async {
         val leaderboardKey = "todo"
         val id = request.session.get("fbid")
         WS.
           url(s"$onorUrl/client/v1/scores/ld/$leaderboardKey/user/$id/provider/facebook?userKey=$userKey").
-          post(request.body.asJson.get).map(res => { //todo move get
-          if(res.status == 200)
+          post(request.body.asJson.get).map(res => {
+          //todo move get
+          if (res.status == 200)
             Ok(res.json)
           else BadRequest(res.body)
         })
       }
-
-      case None => BadRequest("")
-    }
   }
 
 }
