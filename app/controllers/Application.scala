fcad8aa964ed9b99bcb5d1290c3a596445f7db06
Rudolf Markulin <rudolf.markulin@gmail.com>
2013-10-10 16:00:07 +0200

    added Facebook namespace

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,6 +2,7 @@ package controllers
 
 import play.api.mvc._
 import play.api.Logger
+import play.api.libs.json._
 import play.api.libs.ws.WS
 import play.api.libs.concurrent.Execution.Implicits._
 
@@ -12,23 +13,38 @@ object Application extends Controller with GameController {
       Ok(views.html.index())
   }
 
-  def indexPost(gameKey: Int) = Action {
+  def parseNamespace(str: String) =  {
+    val facebookNamespace = str.replaceAll("http://apps.facebook.com/", "").replaceAll("/", "")
+      WS.
+        url(s"$onorUrl/client/v1/games/facebookNamespace/$facebookNamespace?userKey=$userKey").
+        get.map(res => if (res.status == 200) { res.json.\("gameKey").asOpt[Int]} else {None})
+  }
+
+
+  def indexPost() = Action {
     implicit request =>
       Logger.info("INDEX POST")
 
       def callback(gameKey: Int) = s"http://${request.host}/gameKey/$gameKey/facebook/login"
 
-      val settings = Facebook.facebookSettings(gameKey)
-      val sr = request.body.asFormUrlEncoded.get("signed_request").head
-      components.SignedRequestUtils.parseSignedRequest(sr, settings.appSecret) match {
-        case Some(signedRequest) => {
-          Logger.info("GOT SIGNED REQUEST")
-          Redirect(routes.Application.index()).withSession(("fbid", signedRequest.user_id), (GAMEKEY, gameKey.toString))
+      Async{
+      parseNamespace(request.headers("Referer")) map {
+        case Some(gameKey) => {
+          val settings = Facebook.facebookSettings(gameKey)
+          val sr = request.body.asFormUrlEncoded.get("signed_request").head
+          components.SignedRequestUtils.parseSignedRequest(sr, settings.appSecret) match {
+            case Some(signedRequest) => {
+              Logger.info("GOT SIGNED REQUEST")
+              Redirect(routes.Application.index()).withSession(("fbid", signedRequest.user_id), (GAMEKEY, gameKey.toString))
+            }
+            case None => {
+              Logger.info("DIDNT GET SIGNED REQUEST")
+              Ok(views.html.redirect(settings.appId, callback(gameKey), Facebook.fscope))
+            }
+          }
         }
-        case None => {
-          Logger.info("DIDNT GET SIGNED REQUEST")
-          Ok(views.html.redirect(settings.appId, callback(gameKey), Facebook.fscope))
-        }
+        case None => NotFound("game")
+      }
       }
   }
 
