2194057c8f59d4541d6c31754a613af8b6758b5a
Rudolf Markulin <rudolf.markulin@gmail.com>
2013-10-31 16:57:36 +0100

    state proxy cleanup

--- app/controllers/StateService.scala
+++ app/controllers/StateService.scala
@@ -4,6 +4,7 @@ import play.api.mvc._
 import play.api.Logger
 import play.api.libs.ws.WS
 import play.api.libs.concurrent.Execution.Implicits._
+import play.api.libs.json.JsValue
 
 /**
  * Proxy for gamestate/cloudsave service.
@@ -14,54 +15,43 @@ object StateService extends Controller with GameController {
 
   def show = WithGameKeyAndFbid.async(parse.anyContent) {
     implicit request => {
-        val uri = s"$onorUrl/client/v1/games/4pics1word/gk/${request.gameKey}/user/${request.fbid}/provider/facebook?userKey=$userKey"
-        WS.
-          url(uri).
-          get.map(res => {
-          if (res.status == 200)
-            Ok(res.json)
-          else BadRequest(res.body)
-        })
-      }
+      val uri = s"$onorUrl/client/v1/games/4pics1word/gk/${request.gameKey}/user/${request.fbid}/provider/facebook?userKey=$userKey"
+      WS.
+        url(uri).
+        get.map(res => {
+        if (res.status == 200)
+          Ok(res.json)
+        else BadRequest(res.body)
+      })
+    }
   }
 
   def resolveLevel(points: Int) = WithGameKeyAndFbid.async(parse.json) {
     implicit request => {
-        val uri = s"$onorUrl/client/v1/games/4pics1word/gk/${request.gameKey}/user/${request.fbid}/provider/facebook/points/$points?userKey=$userKey"
-        WS.
-          url(uri).
-          put(request.body).map(res => {
-          if (res.status == 200)
-            Ok(res.json)
-          else BadRequest(res.body)
-        })
-      }
-  }
-	
-  def hint(hint: Int) = WithGameKeyAndFbid.async(parse.json) {
-    implicit request => {
-        val uri = s"$onorUrl/client/v1/games/4pics1word/gk/${request.gameKey}/user/${request.fbid}/provider/facebook/hint/$hint?userKey=$userKey"
-        WS.
-          url(uri).
-          put(request.body).map(res => {
-          if (res.status == 200)
-            Ok(res.json)
-          else BadRequest(res.body)
-        })
-      }
-  }
-	
-  def seenLevel() = WithGameKeyAndFbid.async(parse.json) {
-    implicit request => {
-        val uri = s"$onorUrl/client/v1/games/4pics1word/gk/${request.gameKey}/user/${request.fbid}/provider/facebook?userKey=$userKey"
-        WS.
-          url(uri).
-          put(request.body).map(res => {
-          if (res.status == 200)
-            Ok(res.json)
-          else BadRequest(res.body)
-        })
-      }
+      val uri = s"$onorUrl/client/v1/games/4pics1word/gk/${request.gameKey}/user/${request.fbid}/provider/facebook/points/$points?userKey=$userKey"
+      proxyPost(uri, request.body)
+    }
   }
 
+  def hint(hint: Int) = WithGameKeyAndFbid.async(parse.json) {
+    implicit request => {
+      val uri = s"$onorUrl/client/v1/games/4pics1word/gk/${request.gameKey}/user/${request.fbid}/provider/facebook/hint/$hint?userKey=$userKey"
+      proxyPost(uri, request.body)
+    }
+  }
+
+  def seenLevel() = WithGameKeyAndFbid.async(parse.json) {
+    implicit request => {
+      val uri = s"$onorUrl/client/v1/games/4pics1word/gk/${request.gameKey}/user/${request.fbid}/provider/facebook?userKey=$userKey"
+      proxyPost(uri, request.body)
+    }
+  }
+
+  def proxyPost(url: String, body: JsValue) = WS.
+    url(url).
+    put(body).map(res => {
+    if (res.status == 200)
+      Ok(res.json)
+    else BadRequest(res.body)
+  })
 }
