e1b7fccf88bc6863695971c1615e76688f131dda
Rudolf Markulin <rudolf.markulin@gmail.com>
2013-12-20 11:34:50 -0800

    more logging for Facebook authentication

--- app/controllers/Facebook.scala
+++ app/controllers/Facebook.scala
@@ -80,6 +80,9 @@ object Facebook extends Controller {
 
     val authorizationUrl = service.getAuthorizationUrl(EMPTY_TOKEN);
 
+    Logger.error("SR " + request.body.asFormUrlEncoded.get("signed_request"))
+    Logger.error("CODE " + request.queryString.get("code"))
+    Logger.error("ERROR" + request.queryString.get("error_reason"))
 
     try {
         (request.queryString.get("code"), request.queryString.get("error_reason")) match {
