52ac816fb5b6569f538fbea3d7956d6b0f021498
Rudolf Markulin <rudolf.markulin@gmail.com>
2013-12-20 11:46:56 -0800

    fixing additional logging

--- app/controllers/Facebook.scala
+++ app/controllers/Facebook.scala
@@ -80,9 +80,9 @@ object Facebook extends Controller {
 
     val authorizationUrl = service.getAuthorizationUrl(EMPTY_TOKEN);
 
-    Logger.error("SR " + request.body.asFormUrlEncoded.get("signed_request"))
-    Logger.error("CODE " + request.queryString.get("code"))
-    Logger.error("ERROR" + request.queryString.get("error_reason"))
+    Logger.error("SR " + request.body.asFormUrlEncoded.get("signed_request").orElse("DD"))
+    Logger.error("CODE " + request.queryString.get("code").getOrElse(Seq("CC")))
+    Logger.error("ERROR" + request.queryString.get("error_reason").getOrElse("FF"))
 
     try {
         (request.queryString.get("code"), request.queryString.get("error_reason")) match {
