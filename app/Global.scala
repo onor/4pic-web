0123824eaad7475ed284573935ca9e41ed182a34
Rudolf Markulin <rudolf.markulin@gmail.com>
2014-01-09 15:12:50 -0800

    action not found custom page

--- app/Global.scala
+++ app/Global.scala
@@ -1,7 +1,13 @@
 import play.api._
 import play.api.mvc._
 import play.filters.gzip.GzipFilter
+import play.api.mvc.Results._
+import scala.concurrent.Future
+import play.api.libs.concurrent.Execution.Implicits._
+
 
 object Global extends WithFilters(new GzipFilter()) with GlobalSettings {
-  // onStart, onStop etc...
+  override def onHandlerNotFound(request: RequestHeader) = {
+    Future{NotFound(views.html.handlerNotFound())}
+  }
 }
